7237998bc4d4a4e84c157eeebc1e3cec6f9977c2
Nicholas Lucio <nlucio@hmc.edu>
2020-03-09 01:34:58 -0700

    Loaded Background for all screens. Added rudimentary Icons (that don't work currently). Added Style Select.

--- BGAnimations/ScreenTitleMenu background/default.lua
+++
@@ -1,8 +0,0 @@
-local t = Def.ActorFrame{
-    Def.Quad{
-        InitCommand=cmd(scaletoclipped,SCREEN_WIDTH,SCREEN_HEIGHT);
-        OnCommand=cmd(diffuse,Color.Blue;xy,SCREEN_CENTER_X,SCREEN_CENTER_Y;diffusealpha,1);
-    };
-};
-
-return t
